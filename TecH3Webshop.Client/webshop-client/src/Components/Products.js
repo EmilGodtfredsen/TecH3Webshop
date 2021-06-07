be43779369513c14490014905317debbfece8eaf
Emil Godtfredsen <eg@arbitcds.com>
2021-06-07 19:22:27 +0200

    Tried something different for products view

--- TecH3Webshop.Client/webshop-client/src/Components/Products.js
+++ TecH3Webshop.Client/webshop-client/src/Components/Products.js
@@ -4,35 +4,58 @@ import { useLocation } from 'react-router'
 import { Link } from 'react-router-dom';
 
 const Products = () => {
+
     let location = useLocation();
-    if (location.state.products === undefined || location.state.products.length === 0) {
-        return (
-            <h5 className="title"><p className="badge badge-dark bg-secondary">No Products</p></h5>
-        )
-    } else {
-        return (
-            <Container>
+    const listProducts = location.state.products.map((item) =>
+        <div className="card" key={item.id}>
+            <div className="card_img">
+                {item.images.length === 0 ?
+                    <img></img>
+                    :
+                    <img src={item.images[0].imagePath} />
+                }
+            </div>
+            <div className="card_header">
+                <h2>{item.name}</h2>
+                <p className="price">{item.price}<span>$</span></p>
+                <div className="btn">Add to cart</div>
+            </div>
+        </div>
+    );
 
-                {location.state.products.map((val, i) => {
-                    return (
-                        <div key={i}>
+    return (
+        <div className="main_content">
+            {listProducts}
+        </div>
+    )
+    // if (location.state.products === undefined || location.state.products.length === 0) {
+    //     return (
+    //         <h5 className="title"><p className="badge badge-dark bg-secondary">No Products</p></h5>
+    //     )
+    // } else {
+    //     return (
+    //         <Container>
 
-                            <Col>
-                                <Row>
-                                    <Col>
-                                        <ListGroup.Item as={Link} action to={{ pathname: `/product/${val.id}`, state: val }}>
-                                            {val.name}
-                                        </ListGroup.Item>
-                                    </Col>
-                                </Row>
-                            </Col>
+    //             {location.state.products.map((val, i) => {
+    //                 return (
+    //                     <div key={i}>
 
-                        </div>
-                    )
-                })}
-            </Container>
-        )
-    }
+    //                         <Col>
+    //                             <Row>
+    //                                 <Col>
+    //                                     <ListGroup.Item as={Link} action to={{ pathname: `/product/${val.id}`, state: val }}>
+    //                                         {val.name}
+    //                                     </ListGroup.Item>
+    //                                 </Col>
+    //                             </Row>
+    //                         </Col>
+
+    //                     </div>
+    //                 )
+    //             })}
+    //         </Container>
+    //     )
+    // }
 }
 
 export default Products;
